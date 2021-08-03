7be24ac7f8cfb1fa1662a211c2e6b68bb1c30e54
rodgodoyg <80661746+rodgodoyg@users.noreply.github.com>
2021-08-03 11:25:06 -0400

    Delete TestDjango/core directory

--- TestDjango/core/admin.py
+++
@@ -1,7 +0,0 @@
-from core.models import Categoria, Vehiculo
-from django.contrib import admin
-from .models import Categoria, Vehiculo
-
-# Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Vehiculo)
--- TestDjango/core/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Vehiculo
-
-class VehiculosForm(ModelForm):
-
-    
-    class Meta:
-        model = Vehiculo
-        fields = ['patente', 'marca', 'modelo', 'categoria']
